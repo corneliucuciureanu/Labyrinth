a337d351c8fe53981bb48b4ac617152485e0b86a
Corneliu <morfeus.cor@gmail.com>
2017-02-17 17:58:11 +0200

    Model Factory Builder created

---
+++ labyrinth/Labyrinth.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labyrinth;
+import Model.*;
+
+/**
+ *
+ * @author cc@armcloud.eu
+ */
+public class Labyrinth {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Labyrinth Problem");
+        System.out.println(" --- ");
+        
+        LabyrinthModelImpl labyrinth = new LabyrinthModelImpl("FILE");
+        
+    }
+    
+}
---
+++ labyrinth/Parameters.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labyrinth;
+
+/**
+ *
+ * @author cc@armcloud.eu
+ */
+public interface Parameters {
+
+    /**
+     *File from where we read the Labyrinth data as input
+     */
+    public static final String INPUT_FILE = "labyrinthInput1.txt";
+}
